1e40fc2b4ccf789906f624f851527dbb48acafe8
Adalto Picotti Junior <adaltopicotti.jr@gmail.com>
2019-07-30 19:23:13 -0300

    add cors

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
+var cors = require('cors')
 require('dotenv').config()
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 
 
@@ -10,3 +12,4 @@ app.use(express.json())
 app.use('/api', require('./src/routes'))
 
 app.listen(process.env.PORT || 3001)
+console.log('Server starts at port 3001')
